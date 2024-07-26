fe535a44bb0e7ce6bb95e12611024f3c1d9345ad
PeterLombaers <peter@idfuse.nl>
2024-07-26 14:54:54 +0200

    Turn input into stack

--- src/Input.jsx
+++ src/Input.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Opponent from "./input/Opponent.jsx";
 import Rounds from "./input/Rounds.jsx";
 import Type from "./input/NormType.jsx";
+import { List, ListItem, Stack, Paper } from "@mui/material";
 
 export default function Input({
   nRounds,
@@ -20,26 +21,22 @@ export default function Input({
   }
 
   return (
-    <>
-      <h2 className="header header-input">Input</h2>
-      <Type normType={normType} setNormType={setNormType} />
-      <Rounds nRounds={nRounds} setNRounds={setNRounds} />
-      <div className="opponents">
-        <fieldset>
-          <legend>Opponents</legend>
-          <ol>
-            {opponents.map((opponent) =>
-              opponent.round <= nRounds ? (
-                <li className="opponent-item" key={opponent.round}>
-                  <Opponent opponent={opponent} setOpponent={setOpponent} />
-                </li>
-              ) : (
-                false
-              )
-            )}
-          </ol>
-        </fieldset>
-      </div>
-    </>
+    <Paper variant="elevation">
+      <Stack spacing={1}>
+        <Type normType={normType} setNormType={setNormType} />
+        <Rounds nRounds={nRounds} setNRounds={setNRounds} />
+        <List dense>
+          {opponents.map((opponent) =>
+            opponent.round <= nRounds ? (
+              <ListItem>
+                <Opponent opponent={opponent} setOpponent={setOpponent} />
+              </ListItem>
+            ) : (
+              false
+            )
+          )}
+        </List>
+      </Stack>
+    </Paper>
   );
 }
